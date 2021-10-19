6ec190d34693ab007a151dc05c73f6a070cc62d9
Sarah Jameson <meowells@gmail.com>
2021-10-19 20:26:25 +0000

    add more logics like score keep and question counter

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
+const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-button")
+const questionNumberText = document.getElementById("question-number")
+const scoreText = document.getElementById("score")
+
+let score = 0
+let questionNumber = 1
+
+const MAX_QUESTIONS = 10
+
+console.log(questionNumberText)
+
+
 window.onload = sendApiRequest
 
 async function sendApiRequest() {
@@ -12,49 +28,51 @@ function useApiData(data) {
   document.querySelector(".category").innerHTML = `Category is ${data.results[0].category}`
   document.querySelector(".difficulty").innerHTML = `This has a level of ${data.results[0].difficulty}`
   document.querySelector(".question").innerHTML = `${data.results[0].question}`
+  questionNumberText.innerText = questionNumber + "/" + MAX_QUESTIONS
+  scoreText.innerText = score
+
+
+  questionNumber++
 
   let correctAnswerPosition = Math.floor(Math.random() * 4)
 
   console.log(correctAnswerPosition)
-
+  const {correct_answer, incorrect_answers} = data.results[0]
   let correctAnswer = data.results[0].correct_answer
+  const _answers = [correct_answer, ...incorrect_answers]
+  console.log(_answers)
+  const shuffledAnswers = _answers.sort(() => (Math.random() > .5) ? 1 : -1);
+  console.log(shuffledAnswers)
 
-  if (correctAnswerPosition === 0) {
-      document.querySelector("#answer1").innerHTML = data.results[0].correct_answer
-      document.querySelector("#answer2").innerHTML = data.results[0].incorrect_answers[0]
-      document.querySelector("#answer3").innerHTML = data.results[0].incorrect_answers[1]
-      document.querySelector("#answer4").innerHTML = data.results[0].incorrect_answers[2]
+  const answers = Array.from(document.getElementsByClassName('answers'));
+  console.log(answers)
 
-  } else if (correctAnswerPosition === 1) {
-    document.querySelector("#answer1").innerHTML = data.results[0].incorrect_answers[0]
-    document.querySelector("#answer2").innerHTML = data.results[0].correct_answer
-    document.querySelector("#answer3").innerHTML = data.results[0].incorrect_answers[1]
-    document.querySelector("#answer4").innerHTML = data.results[0].incorrect_answers[2]
-  } else if (correctAnswerPosition === 2) {
-    document.querySelector("#answer1").innerHTML = data.results[0].incorrect_answers[0]
-    document.querySelector("#answer2").innerHTML = data.results[0].incorrect_answers[1]
-    document.querySelector("#answer3").innerHTML = data.results[0].correct_answer
-    document.querySelector("#answer4").innerHTML = data.results[0].incorrect_answers[2]
-  } else {
-    document.querySelector("#answer1").innerHTML = data.results[0].incorrect_answers[0]
-    document.querySelector("#answer2").innerHTML = data.results[0].incorrect_answers[1]
-    document.querySelector("#answer3").innerHTML = data.results[0].incorrect_answers[2]
-    document.querySelector("#answer4").innerHTML = data.results[0].correct_answer
-  }
+  answers.forEach((ans,index) => {
+    ans.addEventListener("click", (e) => {
+      console.log("click")
+      if (ans.innerHTML === correct_answer) {
+        console.log("correct")
+        ans.classList.add("correct")
+        addToScore()
+        setTimeout( () => {
+          ans.classList.remove("correct")
+          sendApiRequest(data)
+        }, 1000)
+      } else {
+        console.log("incorrect")
+        ans.classList.add("incorrect")
+        setTimeout( () => {
+          ans.classList.remove("correct")
+          sendApiRequest(data)
+        }, 1000)
+      }
+    })
+    ans.innerHTML = _answers[index]
+  })
 
-  correctAnswer.addEventListener("click", () => {
-    console.log("hell yeah")
-    useApiData(data)
-  });
-}
   
 
-
-const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("next-btn")
-const questionContainerElement = document.getElementById("question-container")
-const questionElement = document.getElementById("question")
-const answerButtonsElement = document.getElementById("answer-button")
+  
 
 startButton.addEventListener("click", startGame);
 
@@ -62,5 +80,11 @@ function startGame() {
   console.log("started");
   startButton.classList.add("hide")
   questionContainerElement.classList.remove("hide")
-};
+}};
+
+function addToScore () {
+  score++
+}
+
+
 
